28463a07f620fe2c1cf6cb195ee838ce7f57bda1
Bhaskar <bkotu6717@gmail.com>
2016-10-15 22:45:30 +0530

    update seed file for data generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Page.delete_all
+
+30.times.each do |x|
+ Page.create(title: "Page #{x+1}", body: "<h1>Page #{x+1} content goes here</h1>")
+end
+
